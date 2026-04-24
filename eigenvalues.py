7bc7c86d09e0da826a543fc9f7761aaa8de5a663
Y_1DEz <90946049+lOriginalD@users.noreply.github.com>
2026-04-24 14:24:59 -0600

    Update eigenvalues.py

--- eigenvalues.py
+++ eigenvalues.py
@@ -3,22 +3,26 @@
 from qr import qr
 from gram_schmidt import matmul
 
-def eigenvals(A:list[list[float]], n:int = 100, tolerance = 1e-10)->list[float]:
+def eigenvals(A: list[list[float]], n: int = 100, tolerance = 1e-10) -> list[float]:
     """
     Realiza n iteraciones del algoritmo QR para calcular 
-    los eigenvalores de A
-
-    Devuelve la estimación de los eigenvalores
+    los eigenvalores de A.
     """
+    # Inicializamos A_k con la matriz original
     A_k = A
-  
+    
     for _ in range(n):
-     # 1. Descomposición QR de la matriz actual
-      Q, R = qr(A_k)
+       #Descomposición de qr en matriz
+        Q, R = qr(A_k)
         
-        # 2. Nueva matriz: multiplicar R y Q en orden inverso
-      A_next = matmul(R, Q)
-
- eigenvalues = [A_k[i][i] for i in range(len(A_k))]
+       # Nueva matriz
+        A_next = matmul(R, Q)
+        
+        # comparación A_k con A_next.
+        
+        A_k = A_next
+        
+    # Los eigenvalores se encuentran en la diagonal de la matriz resultante
+    eigenvalues = [A_k[i][i] for i in range(len(A_k))]
     
     return eigenvalues
